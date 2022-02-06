d098062ba9c917762f3d1485f29659361e02d625
Ruolodi <killer777hunter@gmail.com>
2022-02-06 19:33:48 +0300

    vuelidate

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,205 @@
 <template>
   <div class="container">
- <form @submit.prevent="submit">
+    <form @submit.prevent="submit">
       <div class="card form-control">
         <h1>Форма регистрации</h1>
-      <input  type="text" placeholder="Фамилия*" v-model.trim="lastName">
-      <input  type="text" :style="v$.firstName.$error? styleObject : '' " v-model.trim="firstName" placeholder="Имя*">
-      {{v$.firstName.$error}}
-      <input  type="text" placeholder="Отчество">
-      <input  type="date" placeholder="Дата рождения*">
-      <input  type="text" placeholder="Номер телефона*">
-       <hr/>
-      <select v-model="gender">
-      <option disabled="disabled" value="">Выберите пол</option>
-      <option>Мужской</option>
-      <option>Женский</option>
-      </select>
-      <select multiple  v-model="group">
-      <option disabled value="">Группа клиентов*</option>
-      <option ></option>
-      </select>
-       <select  v-model="doctor">
-      <option disabled value="">Лечащий врач</option>
-      <option>Иванов</option>
-      <option>Захаров</option>
-      <option>Чернышева</option>
-      </select>
-      <hr/>
-      <div class="cardFlex">
-        <p><input class="checkBox" type="checkbox" name="a" value="sms">Не отправлять СМС</p>
+        <small v-if="v$.lastName.$error">Введите фамилию</small>
+        <input
+          type="text"
+          placeholder="Фамилия*"
+          :style="
+            v$.lastName.$error || v$.lastName.mustBeLetter.$invalid
+              ? styleObject
+              : ''
+          "
+          v-model.trim="lastName"
+        />
+        <small v-if="v$.firstName.$error">Введите имя</small>
+        <input
+          type="text"
+          placeholder="Имя*"
+          :style="
+            v$.firstName.$error || v$.firstName.mustBeLetter.$invalid
+              ? styleObject
+              : ''
+          "
+          v-model.trim="firstName"
+        />
+        <input type="text" placeholder="Отчество" />
+        <input type="date" placeholder="Дата рождения*" />
+        <small
+          v-if="
+            v$.number.minLength.$params.min != number.length && v$.number.$dirty
+          "
+          >Неправильный формат номера</small
+        >
+        <input
+          type="number"
+          placeholder="Номер телефона*"
+          :style="
+            v$.number.minLength.$params.min != number.length && v$.number.$dirty
+              ? styleObject
+              : ''
+          "
+          v-model.trim="number"
+        />
+        <hr />
+        <select v-model="selectGender">
+          <option disabled="disabled" value="">Выберите пол</option>
+          <option v-for="items in gender" :key="items">{{ items }}</option>
+        </select>
+        <select
+          v-model="selectGroup"
+          :style="v$.selectGroup.$error ? styleObject : ''"
+          multiple
+        >
+          <option disabled value="">Группа клиентов*</option>
+          <option v-for="items in group" :key="items">{{ items }}</option>
+        </select>
+        <select v-model="selectDoctor">
+          <option disabled value="">Лечащий врач</option>
+          <option v-for="items in doctor" :key="items">{{ items }}</option>
+        </select>
+        <hr />
+        <div class="cardFlex">
+          <p>
+            <input class="checkBox" type="checkbox" name="a" value="sms" />Не
+            отправлять СМС
+          </p>
         </div>
-      <hr/>
+        <hr />
         <h1>Адрес:</h1>
-        <input  type="text" placeholder="Индекс">
-      <input  type="text" placeholder="Страна">
-      <input  type="text" placeholder="Область">
-      <input  type="text" placeholder="Город*">
-      <input  type="text" placeholder="Улица">
-      <input  type="text" placeholder="Дом">
-      <hr/>
-        <h1>Документ удостоверяющий личность:</h1>
-           <select  v-model="document">
-      <option disabled value="">Тип документа</option>
-      <option :value="'birthCertificate'">Свидетельство о рождении</option>
-      <option :value="'passport'">Паспорт</option>
-      <option :value="'driverCertificate'">Водительское удостоверение</option>
-      </select>
-      <div class="passport" v-if="document==='passport'">
-      <input  type="text" placeholder="Серия">
-      <input  type="text" placeholder="Номер">
-      <input  type="text" placeholder="Кем выдан">
-      <input  type="date" placeholder="Дата выдачи*">
-      </div>
-      <div v-else-if="document==='birthCertificate'">
-        <input  type="number" v-model="number" placeholder="Номер">
-      </div>
-      <div v-else-if="document==='driverCertificate'">
-        <input  type="text" placeholder="Номер">
-      </div>
-        <div v-if="v$.firstName.$error"  style="width:100px; height:100px; border:2px solid green">
+        <input type="text" v-model="index" placeholder="Индекс" />
+        <input type="text" v-model="country" placeholder="Страна" />
+        <input type="text" v-model="region" placeholder="Область" />
+        <input
+          type="text"
+          :style="v$.city.$error ? styleObject : ''"
+          placeholder="Город*"
+          v-model="city"
+        />
+        <input type="text" v-model="street" placeholder="Улица" />
+        <input type="text" v-model="home" placeholder="Дом" />
 
+        <hr />
+        <h1>Документ удостоверяющий личность:</h1>
+        <select v-model="document">
+          <option disabled value="">Тип документа</option>
+          <option :value="'birthCertificate'">Свидетельство о рождении</option>
+          <option :value="'passport'">Паспорт</option>
+          <option :value="'driverCertificate'">
+            Водительское удостоверение
+          </option>
+        </select>
+        <div class="passport" v-if="document === 'passport'">
+          <input type="text" v-model="passportSeries" placeholder="Серия" />
+          <input type="text" v-model="passportNumber" placeholder="Номер" />
+          <input type="text" v-model="passportIssued" placeholder="Кем выдан" />
+          <input
+            type="date"
+            :style="v$.passportDate.$error ? styleObject : ''"
+            v-model="passportDate"
+            placeholder="Дата выдачи*"
+          />
+        </div>
+        <div v-else-if="document === 'birthCertificate'">
+          <input type="number" v-model="birthNumber" placeholder="Номер" />
+        </div>
+        <div v-else-if="document === 'driverCertificate'">
+          <input type="text" v-model="driverNumber" placeholder="Номер" />
         </div>
       </div>
       <button class="btn" type="submit">Отправить</button>
- </form>
+      <div class="validate" v-if="sent">Ваши данные отправлены!</div>
+      <div class="errorValidate" v-if="errorSwitch">
+        Пожалуйста проверьте правильность заполнения полей!
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import useVuelidate from '@vuelidate/core'
-import { required, minLength, } from '@vuelidate/validators'
+import useVuelidate from "@vuelidate/core"
+import { required, minLength } from "@vuelidate/validators"
+
+const mustBeLetter = (value) => {
+  return !/[^a-zA-Z]+[^а-яА-Я]/.test(value) //проверка на наличие цифр и спец символов в строке
+}
 
 export default {
-    setup () {
+  setup() {
     return { v$: useVuelidate() }
-    },
-  data(){
-    return{
-      firstName: '',
-      lastName: '',
-      number:'',
-      selected: '',
-      document: '',
-      styleCheck:'',
-      doctor: ['Иванов','Захаров','Чернышева'],
-      group:['VIP','Проблемные','ОМС'],
-      gender:['Мужской','Женский'],
-     styleObject: {
-    borderColor: 'red'
-  }
-
+  },
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      number: "",
+      city: "",
+      selected: "",
+      document: "",
+      passportSeries: "",
+      passportNumber: "",
+      passportIssued: "",
+      passportDate: "",
+      styleCheck: "",
+      selectDoctor: "",
+      selectGender: "",
+      selectGroup: "",
+      index: "",
+      country: "",
+      region: "",
+      street: "",
+      home: "",
+      birthNumber: "",
+      driverNumber: "",
+      errorSwitch: false,
+      sent: false,
+      doctor: ["Иванов", "Захаров", "Чернышева"],
+      group: ["VIP", "Проблемные", "ОМС"],
+      gender: ["Мужской", "Женский"],
+      styleObject: {
+        borderColor: "red",
+      },
     }
   },
-   validations () {
-    return { 
-      firstName: { required }, // Matches this.firstName
-      lastName: { required }, // Matches this.lastName
+  validations() {
+    return {
+      firstName: { required, mustBeLetter },
+      lastName: { required, mustBeLetter },
+      city: { required },
       number: { required, minLength: minLength(11) },
+      selectGroup: { required },
+      passportDate: { required },
     }
   },
-  
-    
-  methods:{
-   async submit(){
-     const result = await this.v$.$validate()
-      if(result===false){
+
+  methods: {
+    async submit() {
+      const result = await this.v$.$validate()
+      if (result === false) {
         this.v$.firstName.$touch()
-        console.log('gggg',this.v$.firstName.required)
+        this.v$.lastName.$touch()
+        this.v$.city.$touch()
+        this.v$.number.$touch()
+        this.v$.selectGroup.$touch()
+        console.log(this.passportDate)
+        this.errorSwitch = true
         return
+      } else {
+        this.errorSwitch = false
+        this.sent = true
       }
-    }
+    },
+  },
+  watch: {
+    number() {
+      if (this.number.length === 1 && this.number !== "7") {
+        this.number = +7 + this.number
+      }
+    },
   },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
